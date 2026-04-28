16dae723ffc8b91f244102a0cd63f62703ad9854
Freddy Kristiansen <fkr@freddy.dk>
2026-04-28 06:09:38 -0400

    Add environment variable outputs to PipelineInitialize.ps1

--- .AL-Go/PipelineInitialize.ps1
+++ .AL-Go/PipelineInitialize.ps1
@@ -4,7 +4,8 @@ Write-Host "--------------------------------------------"
 Write-Host $appBuild
 Write-Host $appRevision
 
-
+Write-Host $env:settings
+Write-Host $env:GITHUB_REF
 
 Set-Variable -Name 'appBuild' -value "1001" -scope 1
 Set-Variable -Name 'appRevision' -value "1000" -scope 1
