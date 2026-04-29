56186b52e724e90c8be8d121af7a738cc24f5801
Freddy Kristiansen <fkr@freddy.dk>
2026-04-28 21:46:06 -0400

    dump all

--- .AL-Go/PipelineInitialize.ps1
+++ .AL-Go/PipelineInitialize.ps1
@@ -4,11 +4,10 @@ Write-Host "--------------------------------------------"
 # Get Repo root
 
 Write-Host "--------------------------------------------"
-1..5 | ForEach-Object {
+1..3 | ForEach-Object {
     Write-Host "Iteration $_"
     try {
-        $settings = Get-Variable -Name "settings" -scope $_
-        $settings | ConvertTo-Json | Out-Host
+        Get-Variable -scope $_ | Out-Host
     }
     catch {
         Write-Host "Variable 'settings' not found in scope $_"
@@ -17,9 +16,6 @@ Write-Host "--------------------------------------------"
 
 Write-Host $appBuild
 Write-Host $appRevision
-
-Write-Host $env:settings
-Write-Host $env:GITHUB_REF
 
 Set-Variable -Name 'appBuild' -value "1001" -scope 1
 Set-Variable -Name 'appRevision' -value "1000" -scope 1
